2bba22df3eb278c24f99497e5cbc9f9a440c8880
Trizzord <trizzordfullfree@gmail.com>
2021-09-22 23:47:07 +0700

    Tugas Agate Chapter 5

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+
+    void Start()
+    {
+        SlingShooter.InitiateBird(Birds[0]);
+    }
+}
